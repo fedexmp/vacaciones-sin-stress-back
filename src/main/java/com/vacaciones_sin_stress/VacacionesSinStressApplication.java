22694ef883ba2e69dab889b5e5fbac8c40807f24
fedexmp <fedexmp96@gmail.com>
2026-03-10 15:25:06 -0300

    Initial Commit

---
+++ src/main/java/com/vacaciones_sin_stress/VacacionesSinStressApplication.java
@@ -0,0 +1,13 @@
+package com.vacaciones_sin_stress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacacionesSinStressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacacionesSinStressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vacaciones_sin_stress/VacacionesSinStressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacaciones_sin_stress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacacionesSinStressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
